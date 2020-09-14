83ce036839e6961635116e85dc845b3f7e0f8fae
bensuRachel <bensu@clevero.com>
2020-09-14 13:37:10 +0530

    Add hacker rank questions

---
+++ src/LeftRotation.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function rotateLeft(times, array) {
+    array = array.slice();
+    while (times--) {
+        var temp = array.shift();
+        array.push(temp)
+    }
+    return array;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = rotateLeft(d, arr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
